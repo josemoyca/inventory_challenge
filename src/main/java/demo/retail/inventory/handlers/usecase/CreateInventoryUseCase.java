a151f51dac86f7f5237cec8e7e831dac5ae90f8c
Jose Moyano <josemoyca@gmail.com>
2023-12-19 08:09:54 -0400

    create inventory use case added inventory DTO added Inventory mapper added

---
+++ src/main/java/demo/retail/inventory/handlers/usecase/CreateInventoryUseCase.java
@@ -0,0 +1,25 @@
+package demo.retail.inventory.handlers.usecase;
+
+import demo.retail.inventory.drivenAdapters.repositories.IInventoryRepository;
+import demo.retail.inventory.mapper.InventoryMapper;
+import demo.retail.inventory.models.DTO.InventoryDto;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+@Service
+@Validated
+public class CreateInventoryUseCase {
+
+    private IInventoryRepository repository;
+
+    public CreateInventoryUseCase(IInventoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public Mono<InventoryDto> apply(InventoryDto inventoryDto) {
+        return repository.save(InventoryMapper.getInventory(inventoryDto))
+                .map(InventoryMapper::getInventoryDto);
+    }
+
+}
---
+++ src/main/java/demo/retail/inventory/models/DTO/InventoryDto.java
@@ -0,0 +1,113 @@
+package demo.retail.inventory.models.DTO;
+
+import demo.retail.inventory.models.Inventory;
+import demo.retail.inventory.models.Product;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class InventoryDto {
+    private String id;
+    private Product product;
+    private Integer availability;
+    private Integer minStockLevel;
+    private Integer maxStockLevel;
+    private Integer reorderPoint;
+    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+
+    public InventoryDto() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Integer availability) {
+        this.availability = availability;
+    }
+
+    public Integer getMinStockLevel() {
+        return minStockLevel;
+    }
+
+    public void setMinStockLevel(Integer minStockLevel) {
+        this.minStockLevel = minStockLevel;
+    }
+
+    public Integer getMaxStockLevel() {
+        return maxStockLevel;
+    }
+
+    public void setMaxStockLevel(Integer maxStockLevel) {
+        this.maxStockLevel = maxStockLevel;
+    }
+
+    public Integer getReorderPoint() {
+        return reorderPoint;
+    }
+
+    public void setReorderPoint(Integer reorderPoint) {
+        this.reorderPoint = reorderPoint;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        InventoryDto that = (InventoryDto) object;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Inventory{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", product=").append(product);
+        sb.append(", availability=").append(availability);
+        sb.append(", minStockLevel=").append(minStockLevel);
+        sb.append(", maxStockLevel=").append(maxStockLevel);
+        sb.append(", reorderPoint=").append(reorderPoint);
+        sb.append(", updatedAt=").append(updatedAt);
+        sb.append(", createdAt=").append(createdAt);
+        sb.append('}');
+        return sb.toString();
+    }
+}
