049a0cc7634e4944ffe0782c4d07ad506e8c6e06
Jose Moyano <josemoyca@gmail.com>
2023-12-19 14:52:34 -0400

    method to get all inventories by pageable added to repository get pageable inventory route and use case added

--- src/main/java/demo/retail/inventory/drivenAdapters/repositories/IInventoryRepository.java
+++ src/main/java/demo/retail/inventory/drivenAdapters/repositories/IInventoryRepository.java
@@ -1,9 +1,13 @@
 package demo.retail.inventory.drivenAdapters.repositories;
 
 import demo.retail.inventory.models.Inventory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
 
 @Repository
 public interface IInventoryRepository extends ReactiveMongoRepository<Inventory, String> {
+    Flux<Inventory> findAllBy(Pageable pageable);
 }
